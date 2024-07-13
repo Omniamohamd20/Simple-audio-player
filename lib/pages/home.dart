a81d70edb926c3627d121ba19e4dda56f2a9ea0a
omnia <omnia.mohamed.elgezar@gmail.com>
2024-07-13 20:03:09 +0300

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -34,53 +34,85 @@ class _HomeState extends State<Home> {
         title: const Text('Home'),
       ),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.blue,
-              ),
-              child: Center(
-                  child: StreamBuilder(
-                      stream: assetsAudioPlayer.realtimePlayingInfos,
-                      builder: (context, snapShots) {
-                        if (snapShots.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-                        return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                assetsAudioPlayer.getCurrentAudioTitle == ' '
-                                    ? 'please play your audio'
-                                    : assetsAudioPlayer.getCurrentAudioTitle,
-                                style: TextStyle(color: Colors.white,fontSize: 20),
-                              ),
-                                SizedBox(
-                                height: 20,
-                              ),
-                              getBtnWidget,
-                            
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Slider(
-                                value: snapShots.data?.currentPosition.inSeconds.toDouble()??0.0, 
-                                min: 0,
-                                max: snapShots.data?.duration.inSeconds.toDouble()??0.0,
-                              onChanged: (value){}
-                              ),
-                              Text(
-'${convertSeconds(snapShots.data?.currentPosition.inSeconds??0)}   /  ${convertSeconds(snapShots.data?.duration.inSeconds??0)}',
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue,
+                ),
+                child: Center(
+                    child: StreamBuilder(
+                        stream: assetsAudioPlayer.realtimePlayingInfos,
+                        builder: (context, snapShots) {
+                          if (snapShots.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+                          return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  assetsAudioPlayer.getCurrentAudioTitle == ' '
+                                      ? 'please play your audio'
+                                      : assetsAudioPlayer.getCurrentAudioTitle,
                                   style: TextStyle(
-                                      color: Colors.white, fontSize: 15))
-                            ]);
-                      }))),
-        ]),
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        snapShots.data?.current?.index==0?null:
+                                      assetsAudioPlayer.previous();
+                                      },
+                                      icon: Icon(Icons.skip_previous),
+                                    ),
+                                    getBtnWidget,
+                                    IconButton(
+                                      onPressed: () {
+                                              snapShots.data?.current?.index==
+                                              (assetsAudioPlayer.playlist?.audios.length??0)-1?
+                                              null:
+                                        assetsAudioPlayer.next(keepLoopMode: false);
+                                      },
+                                      icon: Icon(Icons.skip_next),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Slider(
+                                    value: snapShots
+                                            .data?.currentPosition.inSeconds
+                                            .toDouble() ??
+                                        0.0,
+                                    min: 0,
+                                    max: snapShots.data?.duration.inSeconds
+                                            .toDouble() ??
+                                        0.0,
+                                    onChanged: (value) {}),
+                                Text(
+                                    '${convertSeconds(snapShots.data?.currentPosition.inSeconds ?? 0)}   /  ${convertSeconds(snapShots.data?.duration.inSeconds ?? 0)}',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15))
+                              ]);
+                        }))),
+          ]),
+        ),
       ),
     );
   }
@@ -100,9 +132,9 @@ class _HomeState extends State<Home> {
           shape: CircleBorder(),
         );
       });
-     String convertSeconds(int seconds) {
-      String minutes =(seconds ~/ 60).toString();
-      String secondsStr =(seconds% 60) .toString();
-      return '${minutes.padLeft(2,'0')}:${secondsStr.padLeft(2,'0')}';
-     }
+  String convertSeconds(int seconds) {
+    String minutes = (seconds ~/ 60).toString();
+    String secondsStr = (seconds % 60).toString();
+    return '${minutes.padLeft(2, '0')}:${secondsStr.padLeft(2, '0')}';
+  }
 }
